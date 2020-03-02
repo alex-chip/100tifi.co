c9287d2a3046d7799ee8c3992d12da7654c24ee6
Alexander Chip <alexchip32@gmail.com>
2020-03-01 18:14:15 -0600

    Corrigiendo llamado a la api

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,9 +2,9 @@ const Header = () => {
   const view = `
     <div class="Header-main">
       <div class="Header-logo">
-      <h1>
-        <a href="/">100tifi.com</a>
-      </h1>
+        <h1>
+          <a href="/">100tifi.com</a>
+        </h1>
       </div>
       <div class="Header-nav">
         <a href="#/about/">About</a>
